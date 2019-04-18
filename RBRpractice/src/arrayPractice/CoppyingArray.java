2289168b52eebc46440542c3d0269b3cc4baa4d7
Akashkroy69 <Akashkroy@hotmail.com>
2019-04-18 12:02:15 +0530

    "Array constants can be written only in initializers" Reason for this error has been added in comment.

--- RBRpractice/src/arrayPractice/CoppyingArray.java
+++ RBRpractice/src/arrayPractice/CoppyingArray.java
@@ -3,46 +3,53 @@ package arrayPractice;
 import java.util.Arrays;;
 
 public class CoppyingArray {
-	int[] arrA={1,2,3};
-	int[] arrB;
+	int[] arrA = { 1, 2, 3 };/* To initialize this, either we do this or we can do the following
+	                            if we just want to declare here and want to initialize in seperate line
+	                            arrA= new int[]{4,5,6}
+	                            in other line java doesn't allow this synax for initializing:
+	                            arrA={1,2,3}*/ 
+	
+	int[] arrB;              
+
 	public CoppyingArray() {
-		
-		
+        
 	}
-	public void methodOne()
 	{
+		arrA=new int[] {4,5,6};//THIS IS INITIALIZER BLOCK.
+	}
+
+	public void methodOne() {
 		System.out.println("Implementing with the help of assigning object reference of "
 				+ " arrA into arrB. But this is not called Coppying But works fine");
-		arrB=arrA;
-		
-		for (int i = 0; i < arrB.length; i++) {
-			System.out.println(arrB[i]);
-		}
-	}
-	public void methodTwo()
-	{
-		System.out.println("coppying with the help of for loop");
-		arrB=new int[arrA.length];
-		for (int i = 0; i < arrA.length; i++) {
-			arrB[i]=arrA[i];
-		}
-		
-		for (int i = 0; i < arrB.length; i++) {
-			System.out.println(arrB[i]);
-		}
-	}
-	public void methodThree()
-	{
-		System.out.println("Coppying with the help of Array class's copyOf() method.");
-		arrB=Arrays.copyOf(arrA, arrA.length);
-		
+		arrB = arrA;
 
 		for (int i = 0; i < arrB.length; i++) {
 			System.out.println(arrB[i]);
 		}
 	}
-	public void methodFour()
-	{
+
+	public void methodTwo() {
+		System.out.println("coppying with the help of for loop");
+		arrB = new int[arrA.length];
+		for (int i = 0; i < arrA.length; i++) {
+			arrB[i] = arrA[i];
+		}
+
+		for (int i = 0; i < arrB.length; i++) {
+			System.out.println(arrB[i]);
+		}
+	}
+
+	public void methodThree() {
+		System.out.println("Coppying with the help of Array class's copyOf() method.");
+		arrB = Arrays.copyOf(arrA, arrA.length);
+
+		for (int i = 0; i < arrB.length; i++) {
+			System.out.println(arrB[i]);
+		}
+	}
+
+	public void methodFour() {
 		System.out.println("Coppying with arraycopy method of System class.");
 		System.arraycopy(arrA, 0, arrB, 0, arrA.length);
 
@@ -50,22 +57,23 @@ public class CoppyingArray {
 			System.out.println(arrB[i]);
 		}
 	}
-	public void methodFive()
-	{
+
+	public void methodFive() {
 		System.out.println("Coppying with the help of clone method of Object class of java.");
-		arrB=(int[])arrA.clone();
+		arrB = (int[]) arrA.clone();
 
 		for (int i = 0; i < arrB.length; i++) {
 			System.out.println(arrB[i]);
 		}
 	}
+
 	public static void main(String[] args) {
-		CoppyingArray objectArray=new CoppyingArray();
+		CoppyingArray objectArray = new CoppyingArray();
 		objectArray.methodOne();
 		objectArray.methodTwo();
 		objectArray.methodThree();
 		objectArray.methodFour();
-		
+
 		objectArray.methodFive();
 	}
 
