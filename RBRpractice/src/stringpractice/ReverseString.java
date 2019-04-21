038da01c3900ed77834277b21d50ddd37eb62524
Akashkroy69 <Akashkroy@hotmail.com>
2019-04-21 17:56:46 +0530

    Reversing exactly how ravindra sir has taught about cancatanation of string. but after calling the reverse method it is not printing anything. Need to see why it is not working.

---
+++ RBRpractice/src/stringpractice/ReverseString.java
@@ -0,0 +1,30 @@
+package stringpractice;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	public static String reverse(String stringtobereversed)
+	{
+		String reversedString="";
+		
+		for (int i = stringtobereversed.length()-1; i <= 0; i--) {
+			reversedString=reversedString+stringtobereversed.charAt(i);
+			
+		}
+		
+		 return reversedString;
+		
+	}
+	public static void main(String[] args) {
+		String string;
+		Scanner obScanner=new Scanner(System.in);
+		System.out.println("Enter a String: ");
+		string=obScanner.nextLine();
+		System.out.println(string.length());
+		string=reverse(string);
+		System.out.println("Here is the string after reversing : "+string);
+		obScanner.close();
+		
+	}
+
+}
