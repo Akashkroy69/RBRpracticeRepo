69f3bd30aec0ac00d442aa0df3eff90c4f57af28
Akashkroy69 <Akashkroy@hotmail.com>
2019-04-21 21:30:17 +0530

    new method is added to reverse the string.
    here this method is using default method which is provided
    by the String, StringBuilder, StringBuffer class.

--- RBRpractice/src/stringpractice/ReverseStringUsingStringBuffer.java
+++ RBRpractice/src/stringpractice/ReverseStringUsingStringBuffer.java
@@ -4,29 +4,37 @@ import java.util.Scanner;
 
 public class ReverseStringUsingStringBuffer {
 	public static String reverse(String stringtobereversed) {
-		StringBuffer stringBuffer=new StringBuffer();
+		StringBuffer stringBuffer = new StringBuffer();
 
 		for (int i = stringtobereversed.length() - 2; i <= 0; i--) {
-			
+
 			stringBuffer.append(stringtobereversed.charAt(i));
 
 		}
 
 		return stringBuffer.toString();
 	}
+	//In the following code we are using the default method reverse() which is provided 
+	//by java in String, StringBuffer and StringBuilder class.
 	
+	public static String reverseByDefaultMethodOfStringBuffer(String stringtobereversed) {
+		return (new StringBuffer(stringtobereversed)).reverse().toString();
+		
+	}
+
 	public static void main(String[] args) {
 		String string;
 		Scanner obScanner = new Scanner(System.in);
 		System.out.println("Enter a String: ");
 		string = obScanner.nextLine();
-		//System.out.println(string);
+		// System.out.println(string);
 
-		string = reverse(string);//code to be checked. why it is not printing the string after 
-		                         // reverse function returns. In previous ex this happened too
+		string = reverse(string);// code to be checked. why it is not printing the string after
+									// reverse function returns. In previous ex this happened too
+		string = reverseByDefaultMethodOfStringBuffer(string);
 		System.out.println("Here is the string after reversing : " + string);
 		obScanner.close();
-		
+
 	}
 
 }
