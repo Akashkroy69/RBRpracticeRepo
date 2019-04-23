bd3fa5a52e56698ca96ad8343227a64dd9275eb5
Akashkroy69 <Akashkroy@hotmail.com>
2019-04-24 00:11:36 +0530

    problem in code resolved.

--- RBRpractice/src/stringpractice/ReverseString.java
+++ RBRpractice/src/stringpractice/ReverseString.java
@@ -3,28 +3,29 @@ package stringpractice;
 import java.util.Scanner;
 
 public class ReverseString {
-	public static String reverse(String stringtobereversed)
-	{
-		String reversedString="";
-		
-		for (int i = stringtobereversed.length()-1; i <= 0; i--) {
-			reversedString=reversedString+stringtobereversed.charAt(i);
-			
+	public static String reverse(String stringtobereversed) {
+		String reversedString = "";
+
+		for (int i = stringtobereversed.length() - 1; i >= 0; i--) {
+			reversedString = reversedString + stringtobereversed.charAt(i);
+			/* need to check the program and property of java  later*/
+
 		}
-		
-		 return reversedString;
-		
+
+		return reversedString;
+
 	}
+
 	public static void main(String[] args) {
 		String string;
-		Scanner obScanner=new Scanner(System.in);
+		Scanner obScanner = new Scanner(System.in);
 		System.out.println("Enter a String: ");
-		string=obScanner.nextLine();
-		
-		string=reverse(string);
-		System.out.println("Here is the string after reversing : "+string);
+		string = obScanner.nextLine();
+
+		string = reverse(string);
+		System.out.println("Here is the string after reversing : " + string);
 		obScanner.close();
-		
+
 	}
 
 }
--- RBRpractice/src/stringpractice/ReverseStringUsingStringBuffer.java
+++ RBRpractice/src/stringpractice/ReverseStringUsingStringBuffer.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class ReverseStringUsingStringBuffer {
 	public static String reverse(String stringtobereversed) {
+
 		StringBuffer stringBuffer = new StringBuffer();
 
-		for (int i = stringtobereversed.length() - 2; i <= 0; i--) {
+		for (int i = stringtobereversed.length() - 1; i >= 0; i--) {
 
 			stringBuffer.append(stringtobereversed.charAt(i));
 
@@ -14,12 +15,13 @@ public class ReverseStringUsingStringBuffer {
 
 		return stringBuffer.toString();
 	}
-	//In the following code we are using the default method reverse() which is provided 
-	//by java in String, StringBuffer and StringBuilder class.
-	
+	// In the following code we are using the default method reverse() which is
+	// provided
+	// by java in String, StringBuffer and StringBuilder class.
+
 	public static String reverseByDefaultMethodOfStringBuffer(String stringtobereversed) {
 		return (new StringBuffer(stringtobereversed)).reverse().toString();
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -31,8 +33,20 @@ public class ReverseStringUsingStringBuffer {
 
 		string = reverse(string);// code to be checked. why it is not printing the string after
 									// reverse function returns. In previous ex this happened too
-		string = reverseByDefaultMethodOfStringBuffer(string);
+                                    /*
+                                     *         ^
+                                     *         |
+                                     *         |
+                                     *     PROBLEM RESOLVED: it was just that in function I had written wrong condition case
+                                     *     in for loop in line:10
+                                     * 
+                                     * 
+                                     * */
 		System.out.println("Here is the string after reversing : " + string);
+		System.out.println("Again reversing. This time by default method which is provided by"
+				+ " StringBuilder or StringBuffer class : ");
+		string = reverseByDefaultMethodOfStringBuffer(string);
+		System.out.println(string);
 		obScanner.close();
 
 	}
