01f64956c7619d02e9d940ea5cff7e14e7891466
Akashkroy69 <Akashkroy@hotmail.com>
2019-04-24 21:26:19 +0530

    Two program is being added but
    Sortingcharinstring needs to be checked again.

---
+++ RBRpractice/src/stringpractice/RemoveAllSpace.java
@@ -0,0 +1,39 @@
+package stringpractice;
+
+import java.util.Scanner;
+
+public class RemoveAllSpace {
+
+	public static void removeSpacesMethodOne(String string) {
+		string = string.replaceAll(" ", "");
+		System.out.println("By Method One : "+string);
+	}
+
+	public static void removeSpacesMethodTwo(String string) {
+
+		char[] charArray = string.toCharArray();
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < charArray.length; i++) {
+			if (charArray[i] != ' ') {
+				stringBuilder.append(charArray[i]);
+
+			}
+		}
+
+		string = stringBuilder.toString();
+		System.out.println("By Method 2: "+string);
+
+	}
+
+	public static void main(String[] args) {
+		String string;
+		System.out.println("Enter a String : ");
+		Scanner scanner = new Scanner(System.in);
+		string = scanner.nextLine();
+		removeSpacesMethodOne(string);// (string);//(string);
+		//System.out.println(string);
+		removeSpacesMethodTwo(string);
+
+	}
+
+}
---
+++ RBRpractice/src/stringpractice/SortingCharInString.java
@@ -0,0 +1,63 @@
+package stringpractice;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * 
+ * 
+ *
+ * 
+ * 
+ * 10. Write a java program to sort the given string?
+		Example: input : ravindra
+				 output : aadinrrv
+				 
+*
+* 
+* 
+* 
+* */
+public class SortingCharInString {
+	public static void sortingCharacter(String string) {
+		/*
+		 * int i=string.charAt(0); System.out.println(i);
+		 */
+		int min;
+		char[] sorted = new char[string.length()-1];
+		/*
+		 * int t=string.charAt(0)-string.charAt(1);
+		 * 
+		 * System.out.println(t);
+		 */
+		for (int i = 0; i < string.length(); i++) {
+			min = i;
+			for (int j = i + 1; j < string.length(); j++) {
+
+				if (string.charAt(j) <= string.charAt(min)) {
+					min = j;
+					System.out.println("in this iteration the char which has to be "
+							+ " sorted :"+ string.charAt(min));
+				}
+			}
+			sorted[i] = string.charAt(min);
+		}
+		System.out.println("Here is the sorted array: ");
+		for (char c : sorted) {
+			System.out.print(c);
+			
+		}
+		
+
+	}
+
+	public static void main(String[] args) {
+		String string;
+		System.out.println("Enter a String : ");
+		Scanner scanner = new Scanner(System.in);
+		string = scanner.nextLine();
+		sortingCharacter(string);
+
+	}
+
+}
