b9d45460558cdad60a0d5ccc44943c555039b4f4
Akashkroy69 <47817692+Akashkroy69@users.noreply.github.com>
2019-05-17 13:48:45 +0530

    Generic is being added to Linked list class

--- RBRpractice/src/collectionPractice/LinkedList.java
+++ RBRpractice/src/collectionPractice/LinkedList.java
@@ -2,12 +2,11 @@ package collectionPractice;
 
 import java.util.Scanner;
 
-import javax.xml.stream.events.EndDocument;
-
-public class LinkedList {
-	private static LinkedList headLinkedList;
-	LinkedList nextNode, lastNode;
-	private int data;
+public class LinkedList<E> {
+	E e;
+	private static LinkedList<?> headLinkedList;
+	LinkedList<E> nextNode, lastNode;
+	private E data;
 
 	public LinkedList() {
 		nextNode = null;
@@ -15,21 +14,21 @@ public class LinkedList {
 
 	}
 
-	// At the time of intantiating a new object one node will be created
-	public LinkedList(int data) {
-		this.data = data;
+	// At the time of instantiating a new object one node will be created
+	public LinkedList(E e) {
+		this.data = e;
 		nextNode = null;
 
 	}
 
-	public LinkedList insert(int data) {
-		LinkedList newNodeLinkedList = new LinkedList(data);
+	public LinkedList<E> insert(E e) {
+		LinkedList<E> newNodeLinkedList = new LinkedList<E>(e);
 
 		if (headLinkedList == null) {
 			headLinkedList = newNodeLinkedList;
 			lastNode = newNodeLinkedList;
 		} else {
-			LinkedList tempLinkedList = headLinkedList;
+			LinkedList<E> tempLinkedList = (LinkedList<E>) headLinkedList;
 
 			while (tempLinkedList.nextNode != null) {
 				tempLinkedList = tempLinkedList.nextNode;
@@ -38,16 +37,16 @@ public class LinkedList {
 			tempLinkedList.nextNode = newNodeLinkedList;
 			lastNode = newNodeLinkedList;
 		}
-		return headLinkedList;
+		return (LinkedList<E>) headLinkedList;
 	}
 
-	public void printNodeElements(LinkedList headLinkedList) {
+	public void printNodeElements(LinkedList<E> headLinkedList) {
 		if (headLinkedList == null) {
 			System.out.println("Linked List is empty.");
 			return;
 
 		} else {
-			LinkedList temp = headLinkedList;
+			LinkedList<E> temp = headLinkedList;
 
 			System.out.println(" Here are the elements in the Linked List");
 			while (temp != null) {
@@ -58,35 +57,35 @@ public class LinkedList {
 		return;
 	}
 
-	public LinkedList deleteNode(int data) {
-		LinkedList tmpNode;
+	public LinkedList<E> deleteNode(E data) {
+		LinkedList<E> tmpNode;
 		if (headLinkedList == null) {
 			System.out.println("The Linked list is empty.");
 
-			return headLinkedList;
+			return (LinkedList<E>) headLinkedList;
 		}
 		if (headLinkedList.data == data) {
-			LinkedList tempNode = headLinkedList;
+			tmpNode = (LinkedList<E>) headLinkedList;
 			headLinkedList = headLinkedList.nextNode;
-			return tempNode;
+			return tmpNode;
 		} else if (headLinkedList == lastNode) {
 
 			System.out.println("The last element has been deleted.");
 			headLinkedList = lastNode = null;
-			return headLinkedList;
+			return (LinkedList<E>) headLinkedList;
 
 		} else if (lastNode.data == data) {
 
-			tmpNode = headLinkedList;
+			tmpNode = (LinkedList<E>) headLinkedList;
 			while (!tmpNode.nextNode.equals(lastNode)) {
 				tmpNode = tmpNode.nextNode;
 			}
-			LinkedList retuLinkedList = lastNode;
+			LinkedList<E> retuLinkedList = lastNode;
 			tmpNode.nextNode = null;
 			lastNode = tmpNode;
 			return retuLinkedList;
 		} else {
-			tmpNode = headLinkedList;
+			tmpNode = (LinkedList<E>) headLinkedList;
 			while (tmpNode.nextNode.data != data) {
 				tmpNode = tmpNode.nextNode;
 				if (tmpNode.nextNode == lastNode) {
@@ -95,8 +94,8 @@ public class LinkedList {
 
 				}
 			}
-			LinkedList tempLinkedList2 = tmpNode.nextNode.nextNode;
-			LinkedList returLinkedList = tmpNode.nextNode;
+			LinkedList<E> tempLinkedList2 = tmpNode.nextNode.nextNode;
+			LinkedList<E> returLinkedList = tmpNode.nextNode;
 			tmpNode.nextNode = tempLinkedList2;
 			return returLinkedList;
 
@@ -105,7 +104,7 @@ public class LinkedList {
 
 	public static void main(String[] args) {
 
-		LinkedList linkedList = new LinkedList();
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("How many elements you want to enter: ");
@@ -125,7 +124,7 @@ public class LinkedList {
 		 * scanner.nextInt(); linkedList.insert(data); } //
 		 */
 		System.out.println("---------------------------------------------------");
-		linkedList.printNodeElements(LinkedList.headLinkedList);
+		linkedList.printNodeElements((LinkedList<Integer>) LinkedList.headLinkedList);
 		System.out.println("---------------------------------------------------");
 
 		for (int i = 0; i < n; i++) {
@@ -133,15 +132,14 @@ public class LinkedList {
 			int dataToDelete = scanner.nextInt();
 			linkedList.deleteNode(dataToDelete);
 			System.out.println("Here is the list after deleting the node: ");
-			linkedList.printNodeElements(headLinkedList);
-			if (LinkedList.headLinkedList!=null&&linkedList.lastNode!=null) {
-				System.out.println("\nhead node is:"+LinkedList.headLinkedList.data+"\nlast node is: "+linkedList.lastNode.data);
-				
-			}
-			else {
+			linkedList.printNodeElements((LinkedList<Integer>)headLinkedList);
+			if (LinkedList.headLinkedList != null && linkedList.lastNode != null) {
+				System.out.println("\nhead node is:" + LinkedList.headLinkedList.data + "\nlast node is: "
+						+ linkedList.lastNode.data);
+
+			} else {
 				System.out.println("End of operation.");
 			}
-			
 
 		}
 
