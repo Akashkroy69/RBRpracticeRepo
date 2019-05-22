014e8a96a57e129cf1fd644640902ba9e11e9c9e
Akashkroy69 <47817692+Akashkroy69@users.noreply.github.com>
2019-05-22 13:18:06 +0530

    Need to work on this code

---
+++ RBRpractice/src/collectionPractice/ComparatorImplementation.java
@@ -0,0 +1,14 @@
+package collectionPractice;
+
+import java.util.Comparator;
+
+public class ComparatorImplementation<T> implements Comparator<T> {
+
+	@Override
+	public int compare(T o1, T o2) {
+		
+		
+		return 0;
+	}
+
+}
